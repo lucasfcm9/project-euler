0a992a858fac80fd925a232e791eb6388b18701e
Lucas Fellipe <40740008+lucasfcm9@users.noreply.github.com>
2020-03-08 16:06:08 -0300

    add solved question

---
+++ power_digit_sum.py
@@ -0,0 +1,12 @@
+# What is the sum of the digits of the number 21000?
+
+value = 2**1000
+
+lst = []
+for i in str(value):
+    lst.append(i)
+
+sum = 0
+for i in lst:
+    sum += int(i)
+print(sum)
